be2e47da15ec92db782b57d7f3350bcacf999add
LIAMYUZEDONG <yyyzzddd@gmail.com>
2022-04-05 15:08:13 -0600

    first commit empty app

---
+++ intexnew/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace intexnew.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
